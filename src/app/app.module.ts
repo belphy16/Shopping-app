88f5999657ff268d29e0b9cc7aa465046f75caee
Galina Beyer <galinabeyer@gmail.com>
2017-02-03 17:37:29 +0100

    renaming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {DishesComponent} from './dishes.component';
+import {ListComponent} from './list.component';
+import {DishDetailComponent} from './dish-detail.component';
+import {DashboardComponent} from './dashboard.component';
+import {DishService} from './dish.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DishesComponent,
+    ListComponent,
+    DishDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
