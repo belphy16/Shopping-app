cf9a0716a0fbb885d89402d8a37ca560df12cc96
Galina Beyer <galinabeyer@gmail.com>
2017-03-08 16:29:02 +0100

    shopping list shows ingredients from selected dishes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import {AppRoutingModule} from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import {DishesComponent} from './dishes.component';
@@ -9,6 +13,7 @@ import {ListComponent} from './list.component';
 import {DishDetailComponent} from './dish-detail.component';
 import {DashboardComponent} from './dashboard.component';
 import {DishService} from './dish.service';
+import {DishSearchComponent} from'./dish-search.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +21,14 @@ import {DishService} from './dish.service';
     DashboardComponent,
     DishesComponent,
     ListComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    DishSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
